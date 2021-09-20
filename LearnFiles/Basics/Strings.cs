87e056af90f17cb293739b56aeab446be175c102
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-20 13:10:53 +1000

    3) Strings

---
+++ LearnFiles/Basics/Strings.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LearnCS
+{
+    class Strings
+    {
+        static void  Main(string[] args)
+        {
+            String();
+            Console.ReadLine();
+        }
+        static void String()
+        {
+            string phrase = "Brown jars prevented the mixture from freezing too quickly."; //Set out original phrase
+            int phrase_length = phrase.Length; //the .Length function gets a integer value of the length of the string
+            string UpperPhrase = phrase.ToUpper(); //ToUpper() makes ALL characters go to their upper counterpart
+            string LowerPhrase = phrase.ToLower(); //Opposite to above
+            bool ContainsBrown = phrase.Contains("Brown"); //Checks if the phrase contains "Brown"
+            bool ContainsLowerBrown = phrase.Contains("brown"); //Same as above, HOWEVER, the .Contains function IS CASE SENSETIVE
+            bool Contains_Brown = phrase.ToUpper().Contains("BROWN");//Not case sensetive variant, OOP allows multiple functions on one variable
+
+            char FirstLetter = phrase[0]; //First character in phrase
+            char LastLetter = phrase[(phrase_length - 2)]; //Last character in phrase (-2 because of fullstop)
+
+            Console.WriteLine("The Phrase is: " + phrase);
+            Console.WriteLine("The length of the phrase is " + phrase_length + " Characters long");
+            Console.WriteLine("The phrase in uppercase is: " + UpperPhrase);
+            Console.WriteLine("The lowecase of the phrase is: " + LowerPhrase);
+            Console.WriteLine("Does the phrase contain \"Brown\"?: " + ContainsBrown);
+            Console.WriteLine("Does the phrase contain \"brown\"?: " + ContainsLowerBrown);
+            Console.WriteLine("Does the phrase contain \"brown\"? (Not Case Sensetive): " + Contains_Brown);
+            Console.WriteLine("The first letter is: " + FirstLetter);
+            Console.WriteLine("The last letter is: " + LastLetter);
+
+         }
+    }
+}
