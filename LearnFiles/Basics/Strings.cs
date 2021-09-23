8e174cb5b557671c0bbefc2c686af921a77b6079
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-23 22:24:06 +1000

    3) Strings

--- LearnFiles/Basics/Strings.cs
+++ LearnFiles/Basics/Strings.cs
@@ -15,9 +15,10 @@ namespace LearnCS
             int phrase_length = phrase.Length; //the .Length function gets a integer value of the length of the string
             string UpperPhrase = phrase.ToUpper(); //ToUpper() makes ALL characters go to their upper counterpart
             string LowerPhrase = phrase.ToLower(); //Opposite to above
+           
             bool ContainsBrown = phrase.Contains("Brown"); //Checks if the phrase contains "Brown"
             bool ContainsLowerBrown = phrase.Contains("brown"); //Same as above, HOWEVER, the .Contains function IS CASE SENSETIVE
-            bool Contains_Brown = phrase.ToUpper().Contains("BROWN");//Not case sensetive variant, OOP allows multiple functions on one variable
+            bool Contains_Brown = phrase.ToUpper().Contains("BROWN"); //Not case sensetive variant, OOP allows multiple functions on one variable
 
             char FirstLetter = phrase[0]; //First character in phrase
             char LastLetter = phrase[(phrase_length - 2)]; //Last character in phrase (-2 because of fullstop)
@@ -25,7 +26,6 @@ namespace LearnCS
             int index = phrase.IndexOf("jars"); //Gets the index of "jars", the position the first char is in
             string substring = phrase.Substring(index); //Returns the rest of the phrase after the index
             string second_word = phrase.Substring(6,7); //Returns substrings
-
 
             Console.WriteLine("The Phrase is: " + phrase);
             Console.WriteLine("The length of the phrase is " + phrase_length + " Characters long");
@@ -39,7 +39,6 @@ namespace LearnCS
             Console.WriteLine("The index of \"jars\" is: " + index);
             Console.WriteLine("The Substring of \"jars\" is: " + substring);
             Console.WriteLine("The Substring of string 6-7 is: " + second_word);
-
          }
     }
 }
